d35f652ffcb07432570392e6164f4f08de72aa82
Christopher Madrigal <c4madrig@ucsd.edu>
2025-01-10 19:18:07 -0800

    implement deflateGetDictionary, inflateGetDictionary, deflateParams

--- src/jsMain/kotlin/pako/zlib/deflate.kt
+++ src/jsMain/kotlin/pako/zlib/deflate.kt
@@ -6,6 +6,8 @@ import org.khronos.webgl.Uint8Array
 
 external fun deflateInit(strm: zstream, level: Number): Number
 external fun deflateReset(strm: zstream): Number
+external fun deflateParams(strm: zstream, level: Number, strategy: Number): Number
+external fun deflateGetDictionary(strm: zstream, dictionary: Uint8Array?): Array<Number>
 external fun deflate(strm: zstream, flush: Number): Number
 external fun deflateEnd(strm: zstream): Number
-external fun deflateSetDictionary(strm: zstream, dictionary: Uint8Array): Number
+external fun deflateSetDictionary(strm: zstream, dictionary: Uint8Array?): Number
