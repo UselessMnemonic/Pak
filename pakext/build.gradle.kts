73a44332cf4dae92338cea150ae9e0a71c1cd802
Christopher Madrigal <chrisjmadrigal@gmail.com>
2025-01-12 18:15:24 -0800

    use cargo but more

--- pakext/build.gradle.kts
+++ pakext/build.gradle.kts
@@ -1,23 +1,27 @@
-var profile = "debug"
+import org.gradle.internal.extensions.stdlib.capitalized
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.tomlj:tomlj:1.1.1")
+    }
+}
 
 val cargoHome = providers.systemProperty("CARGO_HOME").map {
     layout.projectDirectory.dir(it)
 }.getOrElse(
     layout.projectDirectory.dir(System.getProperty("user.home")).dir(".cargo")
 )
-val cargo = cargoHome.dir("bin").file("cargo")
+val cargoBin = cargoHome.dir("bin").file("cargo").asFile
+val cargoConfig = with(layout.projectDirectory.dir(".cargo").file("config.toml").asFile.reader()) {
+    org.tomlj.Toml.parse(this)
+}
+
 val rustSrc = layout.projectDirectory.dir("src").asFileTree
-
-data class TargetSpec(
-    val name: String,
-    val target: String
-)
-
-val targets = mutableListOf<TargetSpec>(
-    TargetSpec("Win64", "x86_64-pc-windows-msvc"),
-    //TargetSpec("MacOS64", "x86_64-apple-darwin"),
-    //TargetSpec("Linux64", "x86_64-unknown-linux-gnu")
-)
+val targets = cargoConfig.getTable("target")!!.keySet()
+var profile = "debug"
 
 val updateTask = tasks.create<Exec>("update") {
     group = "cargo"
@@ -25,14 +29,13 @@ val updateTask = tasks.create<Exec>("update") {
     val cargoLock = layout.projectDirectory.file("Cargo.lock")
     inputs.file(cargoToml)
     outputs.file(cargoLock)
-    commandLine(cargo.asFile, "update")
+    commandLine(cargoBin, "update")
 }
-
 
 val buildAllTask = tasks.create("buildAll") {
     group = "cargo"
     for (target in targets) {
-        val tree = layout.buildDirectory.get().dir(target.target).dir(profile).asFileTree.matching {
+        val tree = layout.buildDirectory.get().dir(target).dir(profile).asFileTree.matching {
             include("${project.name}.dll")
             include("lib${project.name}.dylib")
             include("lib${project.name}.so")
@@ -42,13 +45,14 @@ val buildAllTask = tasks.create("buildAll") {
 }
 
 for (target in targets) {
-    val buildTask = tasks.create<Exec>("build${target.name}") {
+    val buildName = target.split('-').joinToString(separator = "") { it.capitalized() }
+    val buildTask = tasks.create<Exec>("build$buildName") {
         group = "cargo"
-        val targetDir = layout.buildDirectory.dir(target.target)
+        val targetDir = layout.buildDirectory.dir(target)
         inputs.files(rustSrc)
         outputs.dir(targetDir)
 
-        val args = mutableListOf(cargo.asFile, "build", "--target=${target.target}", "--target-dir=${layout.buildDirectory.get()}")
+        val args = mutableListOf(cargoBin, "build", "--target=${target}", "--target-dir=${layout.buildDirectory.get()}")
         if (profile == "release") {
             args.add("--release")
         } else if (profile != "debug") {
